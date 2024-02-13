feeb3e7754e89cd8f3df2411ea28b1045e807257
Ibrahim Essam <hemaessam2012@gmail.com>
2024-02-13 09:42:13 +0200

    Build a handle submit, display items, mirror to LS, and restore from LS functions.

--- shopping/script.js
+++ shopping/script.js
@@ -1,91 +1,59 @@
 const shoppingForm = document.querySelector('.shopping');
 const list = document.querySelector('.list');
-const itemsUpdatedEvent = new CustomEvent('itemsUpdate')
 
-// We need an array to hold our state
+// We need to store our state
 let items = [];
-
 function handleSubmit(e) {
-    e.preventDefault();
 
-    const name = e.currentTarget.item.value;
-    if (!name) return;
+    e.preventDefault();
+    const name = e.target.item.value;
+
+    if(!name) return
 
     const item = {
-        name,
         id: Date.now(),
+        name,
         complete: false,
-    };
-    // Push the item into our state
+    }
+    // Push the items into our state
     items.push(item);
-    // Clear the form
+    // Clean the form
     e.target.reset();
-    // fire off a custom event that will tell anyone else who carse that the items have been updated
-    list.dispatchEvent(new CustomEvent('itemsUpdated'));
-    // e.stopPropagation();
+    list.dispatchEvent(new CustomEvent('itemUpdated'))
 }
 
 function displayItems() {
-    console.log(items);
-    const html = items.map(item => `<li class="shopping-item">
-    <input
-        value="${item.id}"
-        type="checkbox"
-        ${item.complete ? 'checked' : ''}>
-    <span class="itemName">${item.name}</span>
-    <button
-        aria-label="Remove ${item.name}"
-        value="${item.id}">
-            &times;
-        </button>
-    </li>`).join('');
+    const html = Array.from(items).map(item => {
+        return `
+            <li class="shopping-item">
+                <input type="checkbox" value="${item.id}">
+                <span class="itemName">${item.name}</span>
+                <button class="btn btn--delete" aria-label="Remove ${item.name}">
+                    &times;
+                </button>
+            </li>
+        `
+    }).join('')
+
     list.innerHTML = html;
 }
 
 function mirrorToLocalStorage() {
-    console.info('Saving items to local storage');
-    localStorage.setItem('items', JSON.stringify(items))
+    console.log('Adding to local storage...');
+    localStorage.setItem("items", JSON.stringify(items));
 }
 
 function restoreFromLocalStorage() {
-    console.log('Restoring from LS');
-    // pull the items from LS
-    const lsItems = JSON.parse(localStorage.getItem('items'));
-    if (lsItems.length) {
-        // items = lsItems;
-        // lsItems.forEach(item => items.push(item));
-        items.push(...lsItems);
-        list.dispatchEvent(new CustomEvent('itemsUpdated'))
+    // pull the items from local storage
+    const lsItems = JSON.parse(localStorage.getItem("items"));
+    if (lsItems?.length) {
+        items.push(...lsItems)
+        list.dispatchEvent(new CustomEvent("itemUpdated"));
     }
-}
-
-function deleteItem(id) {
-    console.log('DELETING ITEM', id);
-    // update our items array without this one
-    items = items.filter(item => item.id !== id)
-    list.dispatchEvent(new CustomEvent('itemsUpdated'))
-}
-
-function markAsComplete(id) {
-    const itemRef = items.find(item => item.id === id);
-    itemRef.complete = !itemRef.complete;
-    list.dispatchEvent(new CustomEvent('itemsUpdated'))
 }
 
 shoppingForm.addEventListener('submit', handleSubmit);
-list.addEventListener('itemsUpdated', displayItems);
-list.addEventListener('itemsUpdated', mirrorToLocalStorage);
-// Event Delegation: We listen for the click on the list <ul> but then delegate the click over the button if that what was clicked</ul>
-list.addEventListener('click', function(e) {
-    const id = parseInt(e.target.value);
-
-    if (e.target.matches('button')) {
-        deleteItem(parseInt(id));
-    }
-
-    if (e.target.matches('input[type="checkbox"]')) {
-        markAsComplete(id);
-    }
-})
+list.addEventListener('itemUpdated', displayItems);
+list.addEventListener('itemUpdated', mirrorToLocalStorage);
 
 restoreFromLocalStorage();
