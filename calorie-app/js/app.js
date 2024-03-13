043b00cd716a01793f8695ebdaf17a3ce1a8c975
Ibrahim Essam <hemaessam2012@gmail.com>
2024-03-13 14:40:04 +0200

    Display remaining calories.

--- calorie-app/js/app.js
+++ calorie-app/js/app.js
@@ -46,7 +46,26 @@ class CalorieTracker {
         caloriesBurnedEl.innerHTML = burned
     }
 
+    #displayCaloriesRemaining() {
+        const caloriesRemainingEl = document.querySelector('#calories-remaining');
+        const progressEl = document.querySelector('#calorie-progress');
 
+        const remaining = this.#calorieLimit - this.#totalCalories;
+        caloriesRemainingEl.innerHTML = remaining;
+
+        const parent = caloriesRemainingEl.closest('.card');
+        if (remaining <= 0) {
+            parent.classList.remove('bg-light');
+            parent.classList.add('bg-danger', 'text-white');
+            progressEl.classList.remove('bg-success');
+            progressEl.classList.add('bg-danger');
+        } else {
+            parent.classList.remove('bg-danger', 'text-white');
+            parent.classList.add('bg-light');
+            progressEl.classList.remove('bg-danger');
+            progressEl.classList.add('bg-success');
+        }
+    }
 
 
 
